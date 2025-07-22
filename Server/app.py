05574f618d0e53431cbdaacaacb986114f260579
BRYAN1309 <bryanthanaya1@gmail.com>
2025-07-22 15:04:00 +0700

    Material api done

--- Server/app.py
+++ Server/app.py
@@ -3,7 +3,6 @@ from controller import user_controller
 from controller import course_controller
 from controller import module_controller, material_controller
 
-
 app = Flask(__name__)
 
 # Define routes
@@ -18,9 +17,10 @@ app.route("/courses/<int:course_id>", methods=["GET"])(course_controller.get_cou
 app.route("/modules", methods=["POST"])(module_controller.create_module)
 app.route("/modules/course/<int:course_id>", methods=["GET"])(module_controller.get_modules_by_course)
 
-# Material routes
+# Endpoint Material
 app.route("/materials", methods=["POST"])(material_controller.create_material)
-app.route("/materials/module/<int:module_id>", methods=["GET"])(material_controller.get_materials_by_module)
+app.route("/materials", methods=["GET"])(material_controller.get_materials)
+app.route("/modules/<int:module_id>/materials", methods=["GET"])(material_controller.get_materials_by_module)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Server/model/material_model.py
+++ Server/model/material_model.py
@@ -6,5 +6,9 @@ class MaterialModel:
         return supabase.table("materials").insert(data).execute()
 
     @staticmethod
+    def get_all_materials():
+        return supabase.table("materials").select("*").order("created_at", desc=True).execute()
+
+    @staticmethod
     def get_materials_by_module(module_id):
-        return supabase.table("materials").select("*").eq("module_id", module_id).execute()
+        return supabase.table("materials").select("*").eq("module_id", module_id).order("created_at", desc=True).execute()
